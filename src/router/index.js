74d3066c95393ff9eeed7d549b2e46fb536ae573
Jay Logelin <jason.logelin@virtustream.com>
2018-08-17 10:44:47 -0300

    Updated build for SPA

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,10 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: 'home',
+      path: '/',
       name: 'home',
       component: () => import('@/views/Home'),
       children: []
