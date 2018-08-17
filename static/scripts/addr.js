827706349fe35cd42af4c439ad8b43c69502fd3f
Jay Logelin <jason.logelin@virtustream.com>
2018-08-16 23:55:09 -0300

    updating build

--- static/scripts/addr.js
+++ static/scripts/addr.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+
+// Rinkeby
 export const ADDRESS = {
 	VGPU: '0xb6dd5b550d094966a1126b5502992f7f4185bd8e',
     VRIG: '0xfb87466b3d7fe8aaa4349f34a1e3be916ffb353d',
@@ -8,3 +10,15 @@ export const ADDRESS = {
     VGPU_MARKET: '0x1ec1d160e372a2afa5c245a5d83f2f74913f4d47',
     VRIG_MARKET: '0xf864aae6f3fa3bce4077039faeddf19e34c9d41a',
 }
+
+/*
+export const ADDRESS = {
+	VGPU: '0xc0d074b731da0804bebd81b067e6ba7e8eb9adf4',
+    VRIG: '0xb1e565001ec56211243a6b7676d61c8fc55b37c5',
+    MITHRIL: '0x5ecf5e59221fea9056e37705e2e83d808489a438',
+    QUARRY: '0xa9239e77f2e7e85456d96c05839f73a19d122f90',    
+    FACTORY: '0x03ee3df245490c6018d0b57f6f705c27e983f289',
+    VGPU_MARKET: '0xeb5d1e388b06924f77e745e910df6580e17d218a',
+    VRIG_MARKET: '0x64ff8137af738f1c84079df3fad8e304bedd0ce7'
+}
+*/
