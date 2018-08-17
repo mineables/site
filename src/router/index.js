92852df103c5b4b0a9dd119d03806665ab57061d
Jay Logelin <jason.logelin@virtustream.com>
2018-08-17 10:59:16 -0300

    Updated build for SPA

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: 'home',
+      path: 'site',
       name: 'home',
       component: () => import('@/views/Home'),
       children: []
