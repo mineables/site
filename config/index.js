b277ec5beb0071040ab843b04bc7def8ae0f9111
Jay Logelin <jason.logelin@virtustream.com>
2018-08-17 00:43:35 -0300

    updated build

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     // assetsPublicPath: './',
     assetsPublicPath: '/site/',
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'site/static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
