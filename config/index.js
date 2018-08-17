b5ff45436f32b403b2ca099f9fd1cf0609aeb80e
Jay Logelin <jason.logelin@virtustream.com>
2018-08-17 01:00:05 -0300

    updated build

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     // assetsPublicPath: './',
-    assetsPublicPath: '/site',
+    assetsPublicPath: '/site/',
     assetsSubDirectory: 'static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
