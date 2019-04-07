285dc478bef72b84e8052907ab2bd0df50689c55
Jay Logelin <jason.logelin@virtustream.com>
2019-04-07 00:51:22 -0300

    git push

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,21 +1,5 @@
 module.exports = {
-
-	/* Sokol testnet */
-	/*
-	"addresses":{
-	  "VGPU": "0x89ccc15d3409bb34defdb40bfcdf3b222f2fe23c",
-	  "VRIG": "0xf0a0c3563ea9237ceb582197711e68dbb7a5c60f",
-	  "MITHRIL": "0x26340cc8229ca059cd38a5a7c094d5061568d0a3",
-	  "QUARRY": "0x1fb2abe9bc999f9f0086f8dd02ed9cea3b1492a7",
-	  "FACTORY": "0x6b3f2801c4c06545405e4ba5c5b3c82075cedca1",
-	  "VGPU_MARKET": "0x33af6603b51871be8393e5fa512c3ee0c92949ce",
-	  "VRIG_MARKET": "0x43af73cec355b18b767a274e9064a73435cb7353"
-	},
-
-    "explorer_url": "https://blockscout.com/poa/sokol/tx/"
-    */
-
-	/* xDai Chain */
+	
 	"addresses": {
 	  "VGPU": "0xeb6b143b4c8580f95a8a8f85b3c192c45af8656a",
 	  "VRIG": "0x4f90fa5fbcdbc092c55e153a39ceea8ffa552e2d",
